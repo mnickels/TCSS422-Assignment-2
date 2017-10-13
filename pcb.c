62093101340c1440a1ef930ca876f008ffbd83cb
Mike Nickels <mc.nickels2@gmail.com>
2017-10-13 11:09:05 -0700

    Sorting out PCB

--- pcb.c
+++ pcb.c
@@ -1,100 +1,142 @@
+/*
+TCSS422 
+Problem #1
+Group Members: 
+Joshua Meigs
+Yaroslav Salo
+*/
 #include "pcb.h"
-#include "prioqueue.h"
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <limits.h>
+#include <time.h>
+unsigned int GLOBAL_PID = 1;
 
-static unsigned int next_pid = 0;
+/************************************************************
+*              CPU_context_s FUNCTION DEFINITIONS           *
+*************************************************************/
 
-PCB_p pcb_new(void) {
-	PCB_p this = (PCB_p) calloc(1, sizeof(PCB_s));
-	// initialize new PCB
-	pcb_init(this);
-	return this;
+int check_pointer(void * ptr) {
+    if(ptr == NULL || ptr == 0) return NO_OBJ_ERR;
+    return NO_ERR;
 }
 
-void pcb_init(PCB_p this) {
-	this->context = (CPU_context_p) calloc(1, sizeof(CPU_context_s));
-	this->context->pc = 0;
-	this->context->ir = 0;
-	this->context->psr = 0;
-	this->context->r0 = 0;
-	this->context->r1 = 0;
-	this->context->r1 = 0;
-	this->context->r1 = 0;
-	this->context->r1 = 0;
-	this->context->r1 = 0;
-	this->context->r6 = 0;
-	this->context->r7 = 0;
-
-	pcb_assign_pid(this);
-	this->state = NEW;
-	this->parent = 0;
-	this->priority = 0;
-	this->mem = 0;
-	this->size = 0;
-	this->channel_no = 0;
+CPU_context_p cpu_context_constructor(void) {
+    CPU_context_p context_ptr = (CPU_context_p) malloc(sizeof(CPU_context_s));;
+    return context_ptr;
 }
 
-void pcb_destroy(PCB_p this) {
-	free(this->context);
-	free(this);
+int cpu_context_deconstructor(CPU_context_p *context_ptr) {
+    if (check_pointer(context_ptr) == NO_OBJ_ERR) return NO_OBJ_ERR; 
+    free(*context_ptr);
+    *context_ptr = NULL; //set the pointer to null for safety. 
+    return NO_ERR;
 }
 
-unsigned int pcb_get_pid(PCB_p this) {
-	return this->pid;
+int cpu_context_init(CPU_context_p context_ptr) {
+    if (check_pointer(context_ptr) == NO_OBJ_ERR) return NO_OBJ_ERR; 
+    for(int i = 0; i < NUM_CONTEXT_REGS; i++) {
+        // 1. cast the struct pointer to a char * so we can do pointer arithmetic 
+        // 2. calculate the offset of every struct member and add that to the pointer
+        // 3. cast the char pointer to an unsign int *
+        // 4. dereference the pointer and set it to the value. 
+        ( *(unsigned int *) ( (char *) context_ptr + sizeof(unsigned int ) * i) ) = 0;   
+    }
+    return NO_ERR;
 }
 
-void pcb_assign_pid(PCB_p this) {
-	this->pid = next_pid++;
+unsigned int cpu_context_get_reg(CPU_context_p context_ptr, unsigned int offset) {
+    if (check_pointer(context_ptr) == NO_OBJ_ERR) return NO_OBJ_ERR;
+    if (offset > sizeof(CPU_context_s)) return OFFSET_ERR;
+    return *(unsigned int *)((char *)context_ptr + offset); 
 }
 
-enum state_type pcb_get_state(PCB_p this) {
-	return this->state;
+int cpu_context_set_reg(CPU_context_p context_ptr, unsigned int offset, int value){
+    if (check_pointer(context_ptr) == NO_OBJ_ERR) return NO_OBJ_ERR;
+    if (offset > sizeof(CPU_context_s)) return OFFSET_ERR;
+    *(unsigned int *)((char *)context_ptr + offset) = value; 
+    return NO_ERR;  
 }
 
-void pcb_set_state(PCB_p this, enum state_type state) {
-	this->state = state;
+char * cpu_context_to_string(CPU_context_p context_ptr) {                    
+    //check pointer
+    if (check_pointer(context_ptr) == NO_OBJ_ERR) {
+        char *err = malloc (sizeof (char));
+        err[0] = NO_OBJ_ERR;
+        return err;
+    }
+
+    char * str = (char *) malloc(sizeof(char) * MAX_CONTEXT_LEN);
+    //format the string. 
+    sprintf(str, "\n* PC:  0x%X\n* IR:  0x%X\n* PSR: 0x%X\n* R0:  0x%X\n* R1:  0x%X\n* R2:  0x%X\n"
+        "* R3:  0x%X\n* R4:  0x%X\n* R5:  0x%X\n* R6:  0x%X\n* R7:  0x%X\n"
+        "*********************"
+        , context_ptr->pc, context_ptr->ir, context_ptr->psr, context_ptr->r0, context_ptr->r1
+        , context_ptr->r2, context_ptr->r3, context_ptr->r4, context_ptr->r5, context_ptr->r6 
+        , context_ptr->r7
+        );
+    return str;
 }
 
-unsigned int pcb_get_parent(PCB_p this) {
-	return this->parent;
+/************************************************************
+*                PCB_s FUNCTION DEFINITIONS                *
+*************************************************************/
+
+PCB_p pcb_constructor(void) {
+    PCB_p pcb_ptr = (PCB_p) malloc(sizeof(PCB_s));
+    CPU_context_p context_ptr = cpu_context_constructor();      // allocate memory for the context.
+    pcb_ptr->context = context_ptr;                             // assign the pcb_context pointer.
+    return pcb_ptr;
+}
+ 
+int pcb_deconstructor(PCB_p *pcb_ptr) {
+    if (check_pointer(pcb_ptr) == NO_OBJ_ERR) return NO_OBJ_ERR; 
+    free(*pcb_ptr);
+    *pcb_ptr = NULL;
+    return NO_ERR;   
 }
 
-unsigned int pcb_get_pc(PCB_p this) {
-	return this->context->pc;
+int pcb_set_pid(PCB_p pcb_ptr){ 
+    if (check_pointer(pcb_ptr) == NO_OBJ_ERR) return NO_OBJ_ERR;
+    pcb_ptr->pid = GLOBAL_PID++;
+    return NO_ERR;
 }
 
-void pcb_set_parent(PCB_p this, unsigned int parent_pid) {
-	this->parent = parent_pid;
+int pcb_init(PCB_p pcb_ptr) {
+    if (check_pointer(pcb_ptr) == NO_OBJ_ERR) return NO_OBJ_ERR; 
+    int r = rand() % 16;                        // generate a random number 0-15
+
+    pcb_set_pid(pcb_ptr);
+    pcb_ptr->state = new;
+    pcb_ptr->parent = 0;                        // ?????????
+    pcb_ptr->priority = r;                      // set the priority randomly
+                                                // TODO change this at somepoint 
+    pcb_ptr->mem = (unsigned char *) pcb_ptr;   // ?????????
+    pcb_ptr->size = 0;                          // ????????? 
+    pcb_ptr->channel_no = 0;                    // ?????????
+    cpu_context_init(pcb_ptr->context);
+    return NO_ERR;
 }
 
-unsigned char pcb_get_priority(PCB_p this) {
-	return this->priority;
-}
+char * pcb_to_string(PCB_p pcb_ptr) {
+    //check the pointer 
+    if (check_pointer(pcb_ptr) == NO_OBJ_ERR) {
+        char *err = malloc (sizeof (char));
+        err[0] = NO_OBJ_ERR;
+        return err;
+    }
+    //allocate temp memory for the context string,
+    char * temp = cpu_context_to_string(pcb_ptr->context);
+    char * str = (char *) malloc(sizeof(char) * MAX_PCB_LEN);
+    //format the string
+    sprintf(str, "*********************"
+        "\n* PID: 0x%X\n* STATE: 0x%X\n* PARENT: 0x%X\n* PRIORITY: 0x%X\n* MEM: 0x%p\n"
+        "* SIZE: 0x%X\n* CHANNEL_NO: 0x%X\n*"
+        "********************"
+    , pcb_ptr->pid, pcb_ptr->state, pcb_ptr->parent, pcb_ptr->priority 
+    , pcb_ptr->mem, pcb_ptr->size, pcb_ptr->channel_no
+    ); 
 
-void pcb_set_priority(PCB_p this, unsigned char priority) {
-	this->priority = (unsigned char) (priority % NUM_PRIORITY_LEVEL);
-}
-
-char * pcb_to_string(PCB_p this, char * s) {
-	char buffer[256]  = "";
-	//sprintf prints formatted data into a buffer/char array (does not allocate space, can overwrite data)
-	sprintf(buffer, "PID: 0x%X, Priority: 0x%X, state: %d, PC: 0x%04X, Mem: 0x%04X, Size: 0x%X, Channel No.: 0x%X",
-			this->pid, this->priority, this->state, this->context->pc, this->mem, this->size, this->channel_no);
-	//TODO: add additional pcb data to to_string
-	strcpy(s, buffer);
-	return s;
-	/*
-	s = "PCB[";
-	strcat(s, "\n  PID:" + this->pid);
-	strcat(s, "\n  state:" + this->state);
-	strcat(s, "\n  parentPID:" + this->parent);
-	strcat(s, "\n  prio:" + this->priority);
-	strcat(s, "\n  mem:" + this->mem);
-	strcat(s, "\n  size:" + this->size);
-	strcat(s, "\n  cannel_no:" + this->channel_no + '\n]');
-	// TODO: strcat(s, "  context:" + this->pid);
-	*/
-}
+    //concatenate the context string to the pcb string.
+    strcat(str, temp);
+    //free the memory of the temporary context string.
+    free(temp);
+    return str;
+}  
